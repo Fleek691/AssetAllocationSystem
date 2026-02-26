bc2856c837abf273cc9d7c532cc654bc85c339ef
Avishek <avishekprasad12345@gmail.com>
2026-02-26 14:21:37 +0530

    Added Asset Assignment Interface

---
+++ AssetAllocationSystem/Services/Interfaces/IAssignmentService.cs
@@ -0,0 +1,11 @@
+﻿using AssetAllocationSystem.DTOs;
+
+namespace AssetAllocationSystem.Services.Interfaces
+{
+    public interface IAssignmentService
+    {
+        Task<AssetAssignmentResponseDto> AssignAssetAsync(AssignAssetDto model);
+        Task<AssetAssignmentResponseDto> ReturnAssetAsync(ReturnAssetDto model);
+        Task<IEnumerable<AssetAssignmentResponseDto>> GetAssignmentsForUserAsync(int userId);
+    }
+}
